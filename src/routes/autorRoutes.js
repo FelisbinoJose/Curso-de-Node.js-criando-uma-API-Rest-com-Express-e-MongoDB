31b9bc8b8800f3224253fb06bf4d2dc90b995802
FelisbinoJose <246760.jose@unibave.net>
2026-01-18 01:39:35 -0300

    feat: adiciona rotas dos autores

---
+++ src/routes/autorRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import autorController from "../controllers/autorController.js"
+
+const routes = express.Router();
+
+routes.get("/autor", autorController.listarAutor);
+routes.get("/autor/:id", autorController.listarAutoresPorId);
+routes.post("/autor", autorController.cadastrarAutor);
+routes.put("/autor/:id", autorController.atualizarAutor);
+routes.delete("/autor/:id", autorController.excluirAutor);
+
+export default routes;
+
